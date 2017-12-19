252791c39e2ac2888716a206966c885b936b3c01
Steven Atkinson <steven.atkinson@gmx.com>
2017-12-19 00:08:58 +0000

    Refactored conditional.

--- lib/day_one/part_one/captcha.rb
+++ lib/day_one/part_one/captcha.rb
@@ -1,9 +1,7 @@
 def captcha(input)
   data = input.to_s.each_char.map(&:to_i)
   data.each_with_index.inject(0) do |sum, (n, i)|
-    if n == data[(i+1) % data.size]
-      sum += n
-    end
+    sum += n if n == data[(i+1) % data.size]
     sum
   end
 end
