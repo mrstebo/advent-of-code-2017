7487948f726737d6cd384abcf77f38d4d237c3bf
Steve Atkinson <steve.atkinson@dootrix.com>
2017-12-19 09:44:54 +0000

    Refactored part one using modules and a simple class with a call method.

--- lib/day_one/part_one/captcha.rb
+++ lib/day_one/part_one/captcha.rb
@@ -1,7 +1,13 @@
-def captcha(input)
-  data = input.to_s.each_char.map(&:to_i)
-  data.each_with_index.inject(0) do |sum, (n, i)|
-    sum += n if n == data[(i+1) % data.size]
-    sum
+module DayOne
+  module PartOne
+    class Captcha
+      def call(input)
+        data = input.to_s.each_char.map(&:to_i)
+        data.each_with_index.inject(0) do |sum, (n, i)|
+          sum += n if n == data[(i+1) % data.size]
+          sum
+        end
+      end
+    end
   end
 end
--- test/day_one/part_one/captcha_test.rb
+++ test/day_one/part_one/captcha_test.rb
@@ -1,20 +1,28 @@
 require_relative '../../test_helper'
 require_relative '../../../lib/day_one/part_one/captcha'
 
-class CaptchaTest < Minitest::Test
-  def test_first_digit_matches_second_and_third_digit_matches_forth
-    assert_equal 3, captcha(1122)
-  end
+module DayOne
+  module PartOne
+    class CaptchaTest < Minitest::Test
+      def setup
+        @tester = DayOne::PartOne::Captcha.new
+      end
 
-  def test_each_digit_matches_the_next
-    assert_equal 4, captcha(1111)
-  end
+      def test_first_digit_matches_second_and_third_digit_matches_forth
+        assert_equal 3, @tester.call(1122)
+      end
 
-  def test_no_digit_matches_the_next
-    assert_equal 0, captcha(1234)
-  end
+      def test_each_digit_matches_the_next
+        assert_equal 4, @tester.call(1111)
+      end
 
-  def test_only_first_digit_matches_last
-    assert_equal 9, captcha(91212129)
+      def test_no_digit_matches_the_next
+        assert_equal 0, @tester.call(1234)
+      end
+
+      def test_only_first_digit_matches_last
+        assert_equal 9, @tester.call(91212129)
+      end
+    end
   end
 end
