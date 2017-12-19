e2621e51e3f94b463a41d9a4a621b82e49ed8161
Steven Atkinson <steven.atkinson@gmx.com>
2017-12-19 21:59:30 +0000

    Renamed n to row.

--- lib/day_two/part_one/checksum.rb
+++ lib/day_two/part_one/checksum.rb
@@ -2,8 +2,8 @@ module DayTwo
   module PartOne
     class Checksum
       def call(input)
-        input.inject(0) do |sum, n|
-          sum += n.max - n.min
+        input.inject(0) do |sum, row|
+          sum += row.max - row.min
           sum
         end
       end
